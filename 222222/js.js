e252c817e57c5e8afb6e3c5f585361b396c2206b
Andrey253 <Andrey253@yandex.ru>
2023-11-14 00:24:41 +0700

    ewr

--- 222222/js.js
+++
@@ -1,8 +0,0 @@
-console.log('log from js');
-
-function e(){
-    var r=document.getElementById('123')
-    console.log(r.innerHTML)
-    r.innerHTML=r.innerHTML+r.innerHTML+'<h1 id="h12">Привет<h1 id="h12">Привет</h1</h1>'
-    console.log(r.innerHTML)
-}
